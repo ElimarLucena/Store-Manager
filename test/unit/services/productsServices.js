7127c6a163b8116a9b3a9f0bdb6c95d1484d1f66
Elimar Lucena <elimarlucenar@gmail.com>
2022-05-30 08:42:49 -0300

    test: testando a função getAll da camada Service, que retorna todos os produtos

--- test/unit/services/productsServices.js
+++ test/unit/services/productsServices.js
@@ -11,45 +11,100 @@ const {
 } = require('../../../services/productsService');
 
 describe('Teste da camada Services relacionada ao produto.', () => {
-  describe('Quando o produto já existe.', () => {
-    const name = 'Martelo de Thor';
+  describe('Testando função que retorna todos os produtos cadastrados no banco de dados', () => {
+    describe('Quando a buscar pelos produtos não tem sucesso.', () => {
+      before(async () => {
+        const result = [];
 
-    it ('retorna um null', async () => {
-      const response = await createNewProduct(name);
+        sinon.stub(models, 'getAll').resolves(result);
+      });
+  
+      after(async () => {
+        models.getAll.restore();
+      });
 
-      expect(response).to.be.a('null');
-      expect(response).to.be.equal(null);
+      it('retorna um array', async () => {
+        const response = await getAll();
+  
+        expect(response).to.be.a('array');
+      });
+
+      it('retorna um array vazio', async () => {
+        const response = await getAll();
+
+        expect(response).to.be.empty;
+      });
+    });
+
+    describe('Quando a buscar pelos produtos tem sucesso.', () => {
+      before(() => {
+        const result = [
+          { id: 1, name: 'Martelo de Thor', quantity: 10 },
+        ];
+
+        sinon.stub(models, 'getAll').resolves(result);
+      });
+  
+      after(() => {
+        models.getAll.restore();
+      });
+
+      it('retorna um array', async () => {
+        const response = await getAll();
+  
+        expect(response).to.be.a('array');
+      });
+
+      it('retorna um array com todos os produtos', async () => {
+        const response = await getAll();
+
+        expect(response).to.have.lengthOf(1);
+        expect(response).to.deep.include({ id: 1, name: 'Martelo de Thor', quantity: 10 });
+      });
     });
   });
 
-  describe('Quando um produto é inserido com sucesso.', () => {
-    const name = "produto";
-    const quantity = 10;
-
-    before(() => {
-      const id = 1;
-
-      sinon.stub(models, 'createNewProduct').resolves({ insertId: id });
+  describe('Testando função que cadastra um novo produto no banco de dados.', () => {
+    describe('Quando o produto já existe.', () => {
+      const name = 'Martelo de Thor';
+  
+      it ('retorna um null', async () => {
+        const response = await createNewProduct(name);
+  
+        expect(response).to.be.a('null');
+        expect(response).to.be.equal(null);
+      });
     });
-
-    after(() => {
-      models.createNewProduct.restore();
-    });
-
-    it ('retorna um objeto', async () => {
-      const response = await createNewProduct(name, quantity);
-
-      expect(response).to.be.a('object');
-    });
-
-    it ('objeto contém o "id" do novo produto inserido', async () => {
-      const response = await createNewProduct(name, quantity);
-
-      expect(response).to.be.property('id');
-      expect(response).to.include({ id: 1 });
-
-      expect(response).to.be.property('name');
-      expect(response).to.include({ name: 'produto'});
+  
+    describe('Quando um produto é inserido com sucesso.', () => {
+      const name = "produto";
+      const quantity = 10;
+  
+      before(() => {
+        const id = 1;
+  
+        sinon.stub(models, 'createNewProduct').resolves({ insertId: id });
+      });
+  
+      after(() => {
+        models.createNewProduct.restore();
+      });
+  
+      it ('retorna um objeto', async () => {
+        const response = await createNewProduct(name, quantity);
+  
+        expect(response).to.be.a('object');
+      });
+  
+      it ('objeto contém o "id" do novo produto inserido', async () => {
+        const response = await createNewProduct(name, quantity);
+  
+        expect(response).to.be.property('id');
+        expect(response).to.include({ id: 1 });
+  
+        expect(response).to.be.property('name');
+        expect(response).to.include({ name: 'produto'});
+      });
     });
   });
 });
