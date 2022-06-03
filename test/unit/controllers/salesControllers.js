b2567e9ba981dc6c9d5a5a5690e1fc02166282fa
Elimar Lucena <elimarlucenar@gmail.com>
2022-06-03 16:45:15 -0300

    test: testando função que atualizar nova venda no banco de dados, na camada Controller

--- test/unit/controllers/salesControllers.js
+++ test/unit/controllers/salesControllers.js
@@ -193,5 +193,92 @@ describe('Teste da camada Controllers relacionada ao Vendas.', () => {
       });
     });
   });
+
+  describe('Testando função que atualizar uma venda no bando de dados.', () => {
+    describe('Quando a venda não é atualizada com sucesso.', () => {
+      const req = {};
+
+      const res = {};
+
+      before(() => {
+        req.params = 1;
+        req.body =  [
+          {
+            productId: 1,
+            quantity: 6
+          }
+        ]
+
+        sinon.stub(services, 'upDateSale').resolves(null);
+
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns({ message: 'Product not found' });
+      });
+
+      after(() => {
+        services.upDateSale.restore();
+      });
+
+      it ('retorna o status 404', async () => {
+        await upDateSale(req, res);
+
+        expect(res.status.calledWith(404)).to.be.equal(true);
+      });
+
+      it ('retorna uma messagem de error.', async () => {
+        await upDateSale(req, res);
+
+        expect(res.json()).to.be.include({ message: 'Product not found' });
+      });
+    });
+
+    describe('Quando a venda é atualizada com sucesso.', () => {
+      const req = {};
+
+      const res = {};
+
+      const updateObj = {
+        saleId: 1,
+        itemUpdated: [
+          {
+            productId: 1,
+            quantity: 6
+          },
+        ],
+      };
+
+      before(() => {
+        req.params = 1;
+        req.body =  [
+          {
+            productId: 1,
+            quantity: 6
+          }
+        ]
+
+        sinon.stub(services, 'upDateSale').resolves(updateObj);
+
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns(updateObj);
+      });
+
+      after(() => {
+        services.upDateSale.restore();
+      });
+
+      it ('retorna o status 200', async () => {
+        await upDateSale(req, res);
+
+        expect(res.status.calledWith(200)).to.be.equal(true);
+      });
+
+      it ('retorna uma messagem de error.', async () => {
+        await upDateSale(req, res);
+
+        expect(res.json()).to.be.property('saleId');
+        expect(res.json()).to.be.a('object');
+      });
+    })
+  });
 });
 
